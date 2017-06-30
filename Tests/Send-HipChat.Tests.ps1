7deb3ff6471c72e919dfb34c7d76798cbbfc5432
Mark Wragg <mark.wragg@gmail.com>
2017-06-30 12:46:52 +0100

    Initial commit

---
+++ Tests/Send-HipChat.Tests.ps1
@@ -0,0 +1,56 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$projectRoot = Split-Path -Parent $here
+$moduleRoot = "$projectRoot\HipChatPS"
+
+. "$moduleRoot\Public\Send-HipChat.ps1"
+
+Describe 'Send-HipChat' {
+
+    Mock Invoke-WebRequest { Import-Clixml "$here\Send-HipChat.invoke-webrequest.xml" }
+    
+    It "should return true" {
+
+        $params = @{
+            message = "Pester test message"
+            room = "Test"
+            apitoken = "c6cS2qXSv1zRyUUXpPsu3bebVF43wx8bvPQK5vg6"
+        }
+
+        Send-HipChat @params | Should Be $true
+    }
+
+    It "should reject the colour blue" {
+
+        $params = @{
+            message = "Pester test message"
+            room = "Test"
+            apitoken = "fakefalsetoken"
+            color = "blue"
+        }
+
+        {send-hipchat @params} | Should Throw
+    }
+}
+
+
+Describe "send-hipchat timeouts" {
+    
+    Mock Invoke-WebRequest {Throw}
+
+    It "should retry 3 additional times" {
+
+        $params = @{
+            message = "Pester test message"
+            room = "Test"
+            apitoken = "fakefalsetoken"
+            retry = 3
+            retrysecs = 1
+            ErrorAction = "SilentlyContinue"
+        }
+
+        send-hipchat @params | Should be $false
+        Assert-MockCalled Invoke-WebRequest -Exactly 4 -ModuleName $moduleName -Scope It
+        
+    }
+
+}
